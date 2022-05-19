51f7dcacd271db7f2e897bdbbfe80cf7438ad64a
Fábio Souza <62041832+fhssouza@users.noreply.github.com>
2022-05-19 11:52:32 -0300

    usando o jakarta persistence(JPA)

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+		
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Fábio Souza");
-		cliente1.setTelefone("91 99999-9999");
-		cliente1.setEmail("fabio@email.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Deyvid Andrade");
-		cliente2.setTelefone("91 99999-9999");
-		cliente2.setEmail("deyvid@email.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
